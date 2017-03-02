912caf3c9898cf93d3704ecf3d90c7d51f1dbec1
Milan Jain <mvmilanjain@gmail.com>
2017-03-03 02:44:16 +0530

    code refactor

--- www/js/agentFirstTimeHandlingCtrl.js
+++ www/js/agentFirstTimeHandlingCtrl.js
@@ -1,4 +1,10 @@
 angular.module('evolveLite')
   .controller('agentFirstTimeHandlingCtrl', function ($scope, utils) {
     $scope.agentGroupData = utils.agentData;
+
+    $scope.getFTR = function () {
+      if(utils.avgFTR) {
+        return utils.avgFTR;
+      }
+    }
   });
